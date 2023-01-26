eceb9e2d8444caebe931bcdb077ba5a705c84f27
Damien Nadé <anvil.github@livna.org>
2023-01-26 22:23:29 +0100

    give those mocks a name

--- test/unit/test_decorator.py
+++ test/unit/test_decorator.py
@@ -1,19 +1,30 @@
 """Retry class unit tests"""
 
-import random
+import string
+from random import randint, choice
 from inspect import isawaitable, iscoroutinefunction
-from unittest.mock import MagicMock, AsyncMock
 import pytest
 
 from kaioretry import Retry, Context
 
+def random_string(length=10):
+    """Return a random string"""
+    letters = string.ascii_lowercase
+    return ''.join(choice(letters) for _ in range(length))
 
-def test_retry(exception):
+
+def configure_callable_mock(mock):
+    """Configure a mock to act as a regularly-defined function"""
+    mock.__name__ = random_string()
+
+
+def test_retry(exception, mocker):
     """Test for retry until success"""
-    result = random.randint(1, 10000000)
-    side_effect = [exception] * random.randint(1, 10) + [result]
+    result = randint(1, 10000000)
+    side_effect = [exception] * randint(1, 10) + [result]
 
-    mock = MagicMock(side_effect=side_effect)
+    mock = mocker.MagicMock(side_effect=side_effect)
+    configure_callable_mock(mock)
 
     retry = Retry(exception)
     retryable = retry.retry(mock)
@@ -23,32 +34,33 @@ def test_retry(exception):
     assert mock.call_count == len(side_effect)
 
 
-@pytest.mark.parametrize("mock", (MagicMock(), AsyncMock()))
-async def test_aioretry(exception, mock):
+async def test_aioretry(exception, any_mock):
     """Test that Retry.aioretry works with both functions and
     coroutine functions.
     """
-    result = random.randint(1, 10000000)
-    side_effect = [exception] * random.randint(1, 10) + [result]
+    result = randint(1, 10000000)
+    side_effect = [exception] * randint(1, 10) + [result]
 
-    mock.side_effect = side_effect
+    any_mock.side_effect = side_effect
+    configure_callable_mock(any_mock)
 
     retry = Retry(exception)
-    retryable = retry.aioretry(mock)
+    retryable = retry.aioretry(any_mock)
     coro = retryable()
 
-    assert retryable is not mock
+    assert retryable is not any_mock
     assert isawaitable(coro)
     assert await coro == result
-    assert mock.call_count == len(side_effect)
+    assert any_mock.call_count == len(side_effect)
 
 
 async def test_retry_final_failure(exception, mock, assert_result):
     """When tries are exhausted, the last raised exception should be
     propagated by the decorator"""
-    side_effect = [exception() for _ in range(random.randint(1, 10))]
+    side_effect = [exception() for _ in range(randint(1, 10))]
 
     mock.side_effect = side_effect
+    configure_callable_mock(mock)
 
     retry = Retry(exception, context=Context(tries=len(side_effect)))
     retryable = retry(mock)
@@ -61,12 +73,9 @@ async def test_retry_final_failure(exception, mock, assert_result):
     assert exc_info.value is side_effect[-1]
 
 
-@pytest.mark.parametrize("mock_cls", (MagicMock, AsyncMock))
-async def test_retry___call__(exception, mock_cls):
+async def test_retry___call__(exception, any_mock):
     """Test that __call__ return a same-nature decorated function"""
-    mock = mock_cls()
     retry = Retry(exception)
+    retryable = retry(any_mock)
 
-    retryable = retry(mock)
-
-    assert iscoroutinefunction(retryable) == iscoroutinefunction(mock)
+    assert iscoroutinefunction(retryable) == iscoroutinefunction(any_mock)
