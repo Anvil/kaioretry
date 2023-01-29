8d7d5ea77665bcee7908b704a19f9e0da6554910
Damien Nadé <anvil.github@livna.org>
2023-01-29 02:59:32 +0100

    refactor decorator tests and fixtures

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,33 +1,51 @@
 """Common unit tests fixtures"""
 
 import inspect
-from unittest.mock import MagicMock
+import string
+from random import choice
 
 import pytest
 import pytest_cases
 
+from kaioretry import Retry
 
 # This is a copy of Lib/unit/mock.py from github, since my local
 # python 3.11.1 does not have iso-functionning MagicMock and
 # AsyncMock. This copy includes the fix for
 # create_autospec(async_def), which is addressed by:
 # https://github.com/python/cpython/pull/94962
-from .mock import AsyncMock, create_autospec
+from .mock import create_autospec
 
 
-def __f(x=1, y=2, z=3):
+def _sync(first=1, second=2, third=3):
+    """A stupid sync function"""
+    # pylint: disable=unused-argument
     return 10
 
 
-async def __g(x=1, y=2, z=3):
+async def _async(first=1, second=2, third=3):
+    """A stupid async function"""
+    # pylint: disable=unused-argument
     return 10
 
 
-@pytest.fixture(params=(__f, __g))
-def any_mock(request):
-    """Provide a {Magic,Async}mock"""
-    mock = create_autospec(request.param)
+def random_string(length=10):
+    """Return a random string"""
+    return ''.join(choice(string.ascii_lowercase) for _ in range(length))
+
+
+def auto_spec(func):
+    """Return an autospec-ed mock from a given function"""
+    mock = create_autospec(func)
+    mock.__qualname__ = mock.__name__ = random_string()
+    mock.__doc__ = random_string()
     return mock
+
+
+@pytest.fixture(params=(_sync, _async))
+def any_func(request):
+    """Provide a mock impersonating a function or async function"""
+    return auto_spec(request.param)
 
 
 @pytest.fixture
@@ -61,17 +79,19 @@ async def assert_sync_result(result, expected):
     assert result == expected
 
 
-@pytest_cases.fixture(unpack_into="mock, assert_result")
-def just_a_mock(any_mock):
-    # pylint: disable=redefined-outer-name
-    """Give a sync or async mock, with a matching coroutine function
-    able to check its result.
-    """
-    if isinstance(any_mock, AsyncMock):
-        yield any_mock, assert_async_result
-        assert any_mock.await_count == any_mock.call_count
-    else:
-        yield any_mock, assert_sync_result
+@pytest_cases.fixture(
+    unpack_into="decorator, func, assert_result",
+    params=((Retry.retry, _sync, assert_sync_result),
+            (Retry.aioretry, _sync, assert_async_result),
+            (Retry.aioretry, _async, assert_async_result)),
+    ids=("sync-sync", "async-sync", "async-async"))
+def supported_cases(request):
+    """Provides the working combos decorator / func / validation of result"""
+    decorator, func, assert_result = request.param
+    mock = create_autospec(func)
+    yield decorator, mock, assert_result
+    if inspect.iscoroutinefunction(func):
+        assert mock.await_count == mock.call_count
 
 
 @pytest.fixture
