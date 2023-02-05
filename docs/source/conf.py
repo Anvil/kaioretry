ddf243633b48d58eb74d7da3f9ec3aa1a369bad7
Damien Nadé <anvil.github@livna.org>
2023-02-05 21:08:30 +0100

    use sphinx_autodoc_typehints extension for more readability

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,10 @@ release = '0.7.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx'
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx_autodoc_typehints'
 ]
 
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
