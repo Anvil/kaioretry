809587078ff7f3fb0726dc2d6bca1d2aa182ee81
Damien Nadé <anvil.github@livna.org>
2023-12-02 02:22:30 +0100

    add mypy tests for named arguments

---
+++ test/static_analysis/34_main_aioretry_named_args.py
@@ -0,0 +1,18 @@
+"""Retry object, with __call__, no param"""
+
+import asyncio
+from kaioretry import aioretry
+
+@aioretry(exceptions=Exception, tries=1)
+async def func(param1: int) -> float:
+    """ one of """
+    return 1 / param1
+
+
+async def func2(param2: int) -> float:
+    """Anything using the first func"""
+    return 3 + await func(param2)
+
+
+if __name__ == "__main__":
+    assert asyncio.run(func(1)) == 1
