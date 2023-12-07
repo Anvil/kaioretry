fc35759c27f4832fc1e31bb0ceaa8b2d815c8c11
Damien Nadé <anvil.github@livna.org>
2023-12-07 23:18:41 +0100

    Add working retry decorator static analysis test files

---
+++ test/static_analysis/retry_built_direct_sync_param_any_str.py
@@ -0,0 +1,28 @@
+'''Test retry_built_direct_sync_param_any_str.py '''
+
+# pylint: disable=unused-import, unused-argument, invalid-name, R0801
+
+
+import asyncio
+from typing import Any
+
+from kaioretry import Retry, Context
+
+
+@Retry(
+    exceptions=(ValueError, NotImplementedError),
+    context=Context(tries=5, delay=2)).retry
+def func(x: Any, y: Any) -> str:
+    ''' ... '''
+    return 'return_value'
+
+
+async def use_decoration(parameter: str) -> str:
+    ''' obtain result and use it '''
+    result = func(1, 2)
+    assert isinstance(result, str)
+    return f"parameter is {parameter}. result is {result}"
+
+
+if __name__ == "__main__":
+    print(asyncio.run(use_decoration("value")))
