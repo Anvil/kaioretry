574ee1c0544b4d0aa840bb29477ca313fc119319
Damien Nadé <anvil.github@livna.org>
2023-12-12 23:55:49 +0100

    Add newly-working static-analysis test files

---
+++ test/static_analysis/aioretry_main_by_call_unnamed_sync_args_any_str.py
@@ -0,0 +1,28 @@
+'''Test aioretry_main_by_call_unnamed_sync_args_any_str.py '''
+
+# pylint: disable=unused-import, unused-argument, invalid-name, R0801
+
+
+import asyncio
+from typing import Any
+from collections.abc import Callable, Awaitable
+from mypy_extensions import VarArg, KwArg
+from kaioretry import retry, aioretry, Retry, Context
+
+
+def func(*args: Any) -> str:
+    ''' ... '''
+    return 'return_value'
+
+wrapped: Callable[[VarArg(Any)], Awaitable[str]] = aioretry(Exception, 2)(func)
+
+
+async def use_decoration(parameter: str) -> str:
+    ''' obtain result and use it '''
+    result = await wrapped(1, 2)
+    assert isinstance(result, str)
+    return f"parameter is {parameter}. result is {result}"
+
+
+if __name__ == "__main__":
+    print(asyncio.run(use_decoration("value")))
